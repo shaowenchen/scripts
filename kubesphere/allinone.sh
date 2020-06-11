f26cd2e56f4fcfb1d4a1b64d1b1ee688f71d7aeb
shaowenchen <mail@chenshaowen.com>
2020-06-11 16:12:14 +0800

    mirror

--- kubesphere/allinone.sh
+++ kubesphere/allinone.sh
@@ -14,6 +14,6 @@ getInstaller
 
 # chooseModule /kubesphere/config-sample.yaml
 
-curl https://raw.githubusercontent.com/shaowenchen/scripts/master/kubesphere/allinone.yaml > allinone
+curl https://raw.githubusercontent.com/shaowenchen/scripts/master/kubesphere/allinone.yaml > allinone.yaml
 
 echo -e 'yes\n' | /kubesphere/kk create cluster -f allinone.yaml
