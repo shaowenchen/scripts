b21cc97ecc3a01cd5d34d86dfe8a8c79bc5d18db
shaowenchen <43693241+shaowenchen@users.noreply.github.com>
2021-01-26 20:19:50 +0800

    Update init.sh

--- harbor-https/init.sh
+++ harbor-https/init.sh
@@ -1,3 +1,3 @@
-rm -rf /etc/docker/certs.d/core.harbor.dev.chenshaowen.com/ 
-mkdir -p /etc/docker/certs.d/core.harbor.dev.chenshaowen.com/
-mv ca.crt  harbor.dev.chenshaowen.com.cert harbor.dev.chenshaowen.com.key -t /etc/docker/certs.d/core.harbor.dev.chenshaowen.com/
+rm -rf /etc/docker/certs.d/harbor.dev.chenshaowen.com/ 
+mkdir -p /etc/docker/certs.d/harbor.dev.chenshaowen.com/
+mv ca.crt  harbor.dev.chenshaowen.com.cert harbor.dev.chenshaowen.com.key -t /etc/docker/certs.d/harbor.dev.chenshaowen.com/
